68f616dfe2f818044b0b52f19b51485c7391d5a7
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2025-11-29 20:10:05 -0600

    cambios en main para pruebas

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,21 @@ int main ()
 {   
     Grafo grafo(50);
     grafo.cargarDesdeArchivo("data/grafo.txt");
-    // grafo.imprimirGrafo();
+    grafo.imprimirGrafo();
+
+    int inicioPruebas = 5;
+    int destinoPruebas = 1;
 
     AlgoritmosGrafo algoritmos(grafo);
-    vector<int> rutaBFS = algoritmos.busquedaAnchura(17, 3);
-    vector<int> rutaDFS = algoritmos.busquedaProfundidad(17, 3);
+    vector<int> rutaBFS = algoritmos.busquedaAnchura(inicioPruebas, destinoPruebas);
+    vector<int> rutaDFS = algoritmos.busquedaProfundidad(inicioPruebas, destinoPruebas);
+    vector<int> rutaPrim = algoritmos.algoritmoPrim(inicioPruebas, destinoPruebas);
 
     algoritmos.imprimirRutaConCosto(rutaBFS);
     
     algoritmos.imprimirRutaConCosto(rutaDFS);
+
+    algoritmos.imprimirRutaConCosto(rutaPrim);
     
 
     return 0; 
