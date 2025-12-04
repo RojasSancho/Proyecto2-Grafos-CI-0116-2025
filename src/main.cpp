0f8cd4af8f7326730e8564c9b7ab5c56b5b929c1
BrandonAlfaroSaborio <brandonalfarosaborio@gmail.com>
2025-12-04 11:56:17 -0600

    -Se adapto eel main para que use la interfaz grafica como output

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,28 @@
-#include "grafo.h"
-#include "algoritmosGrafo.h"
+#include "../include/grafo.h"
+#include "../include/jugador.h"
+#include "../include/algoritmosGrafo.h"
+#include "../include/interfaz.h"
 
 using namespace std;
 
-int main () 
-{   
-    Grafo grafo(50);
+int main() 
+{
+    // Crea grafo y lo carga
+    Grafo grafo(48);
     grafo.cargarDesdeArchivo("data/grafo.txt");
-    // grafo.imprimirGrafo();
 
-    int inicioPruebas = 13;
-    int destinoPruebas = 0;
+    // Crear jugador
+    // Jugador(grafo, bateriaInicial, ubicacionInicial, recursosInicial)
+    Jugador jugador(grafo, 100, grafo.getBaseID(), 100);
 
+    // Crea modulo de algoritmos
     AlgoritmosGrafo algoritmos(grafo);
-    vector<int> rutaBFS = algoritmos.busquedaAnchura(inicioPruebas, destinoPruebas);
-    vector<int> rutaDFS = algoritmos.busquedaProfundidad(inicioPruebas, destinoPruebas);
-    vector<int> rutaPrim = algoritmos.algoritmoPrim(inicioPruebas, destinoPruebas);
 
-    cout << "\nBFS: " << endl;
-    algoritmos.imprimirRutaConCosto(rutaBFS);
-    cout << "\nDFS: " << endl;
-    algoritmos.imprimirRutaConCosto(rutaDFS);
-    cout << "\nPrim (Usando MST): " << endl;
-    algoritmos.imprimirRutaConCosto(rutaPrim);
-    
-    cout << "\nFloyd-Warshall: " << endl;
-    // Calcula todas las distancias
-    auto [distancias, predecesores] = algoritmos.algoritmoFloydWarshall();
-    // Obtiene una ruta especifica 
-    vector<int> rutaFloyd = algoritmos.rutaFloydWarshall(inicioPruebas, destinoPruebas, predecesores);
-    algoritmos.imprimirRutaConCosto(rutaFloyd);
-    // Importante para verificar valores 
-    cout << "Distancia minima (verificacion con matriz de costos): " << distancias[inicioPruebas][destinoPruebas] << endl; 
+    // Crea interfaz grafica
+    Interfaz gui(grafo, jugador, algoritmos);
 
-    // Prueba Dijkstra
-    vector<int> rutaDijkstra = algoritmos.algoritmoDijkstra(inicioPruebas, destinoPruebas);
-    cout << "\nDijkstra: " << endl;
-    algoritmos.imprimirRutaConCosto(rutaDijkstra);
+    // Ejecuta el juego
+    gui.ejecutar();
 
-    return 0; 
+    return 0;
 }
